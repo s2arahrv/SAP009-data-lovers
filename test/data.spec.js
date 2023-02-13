548cfa9a488ae2c8e59c67b0b40035448296b8cd
marianalopesdev <marianalopes.dev@gmail.com>
2023-02-12 23:14:13 -0300

    test examples modified

--- test/data.spec.js
+++ test/data.spec.js
@@ -12,16 +12,16 @@ describe('films.alphabeticOrderFilter', () => {
 
   it('returns an array in alphabetical order by title', () => {
     const input = [
-      { title: 'dog', year: 2020 },
-      { title: 'cat', year: 2019 },
-      { title: 'apple', year: 2021 },
-      { title: 'banana', year: 2022 },
+      { title: 'hope', year: 2023 },
+      { title: 'chaos', year: 2018 },
+      { title: 'star', year: 2003 },
+      { title: 'coup', year: 2016 },
     ];
     const expectedOutput = [
-      { title: 'apple', year: 2021 },
-      { title: 'banana', year: 2022 },
-      { title: 'cat', year: 2019 },
-      { title: 'dog', year: 2020 },
+      { title: 'chaos', year: 2018 },
+      { title: 'coup', year: 2016 },
+      { title: 'hope', year: 2023 },
+      { title: 'star', year: 2003 },
     ];
     expect(films.alphabeticOrderFilter(input)).toEqual(expectedOutput);
   });
