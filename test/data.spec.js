abff598e405679dbd31bbf51b5d1902b6bfd2a72
marianalopesdev <marianalopes.dev@gmail.com>
2023-02-12 23:10:57 -0300

    first test implemented

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,29 @@
-import { example, anotherExample } from '../src/data.js';
+import { films } from '../src/data.js';
 
+describe('films', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof films).toBe('object');
+  })});
 
-describe('example', () => {
+describe('films.alphabeticOrderFilter', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof films.alphabeticOrderFilter).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns an array in alphabetical order by title', () => {
+    const input = [
+      { title: 'dog', year: 2020 },
+      { title: 'cat', year: 2019 },
+      { title: 'apple', year: 2021 },
+      { title: 'banana', year: 2022 },
+    ];
+    const expectedOutput = [
+      { title: 'apple', year: 2021 },
+      { title: 'banana', year: 2022 },
+      { title: 'cat', year: 2019 },
+      { title: 'dog', year: 2020 },
+    ];
+    expect(films.alphabeticOrderFilter(input)).toEqual(expectedOutput);
   });
 });
 
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
